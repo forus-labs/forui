032f5ebf23ba5c80980152c8dd19b3a522cb39e7
Joe Kawai <kawaijoe@users.noreply.github.com>
2024-05-09 14:14:20 +0800

    Update forui/lib/src/theme/font/text_style_builder.dart
    Co-authored-by: Matthias Ngeo <matthiasngeo@gmail.com>

--- forui/lib/src/theme/font/text_style_builder.dart
+++ forui/lib/src/theme/font/text_style_builder.dart
@@ -11,12 +11,11 @@ import 'package:forui/forui.dart';
 /// * Word spacing
 extension type TextStyleBuilder._(TextStyle style) implements TextStyle {
   /// Creates a [TextStyle] that inherits the properties from the given [FFontData].
-  TextStyleBuilder.inherit(FFontData data, TextStyle style)
-      : style = TextStyle(
-    fontSize: _calculateFactor(style.fontSize, data.fontSize),
-    letterSpacing: _calculateFactor(style.letterSpacing, data.letterSpacing),
-    wordSpacing: _calculateFactor(style.wordSpacing, data.wordSpacing),
-    height: _calculateFactor(style.height, data.height),
+  TextStyleBuilder(TextStyle base, FFontData data): style = TextStyle(
+    fontSize: _calculateFactor(base.fontSize, data.fontSize),
+    letterSpacing: _calculateFactor(base.letterSpacing, data.letterSpacing),
+    wordSpacing: _calculateFactor(base.wordSpacing, data.wordSpacing),
+    height: _calculateFactor(base.height, data.height),
   );
 
   static double? _calculateFactor(double? value, double factor) => value == null ? null : value * factor;
