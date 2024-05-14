42b4bbb70f498e16b394ff7ab7c472a72b813346
Joe <kawaijoe@gmail.com>
2024-05-14 19:45:47 +0800

    Add font family field

--- forui/lib/src/theme/font/font_data.dart
+++ forui/lib/src/theme/font/font_data.dart
@@ -7,6 +7,9 @@ import 'package:forui/forui.dart';
 /// This class contains scalar values that are multiplied with the corresponding properties on [TextStyle] to ensure
 /// that various fonts are scaled consistently across the application.
 class FFontData {
+  /// The font family.
+  final String fontFamily;
+
   /// A value used to scale [TextStyle.fontSize].
   final double fontSizeScalar;
 
@@ -21,6 +24,7 @@ class FFontData {
 
   /// Creates a [FFontData].
   const FFontData({
+    this.fontFamily = 'Inter',
     this.fontSizeScalar = 1,
     this.letterSpacingScalar = 1,
     this.wordSpacingScalar = 1,
@@ -29,12 +33,14 @@ class FFontData {
 
   /// Creates a copy of this [FFontData] with the given properties replaced.
   FFontData copyWith({
+    String? fontFamily,
     double? fontSizeScalar,
     double? letterSpacingScalar,
     double? wordSpacingScalar,
     double? heightScalar,
   }) =>
       FFontData(
+        fontFamily: fontFamily ?? this.fontFamily,
         fontSizeScalar: fontSizeScalar ?? this.fontSizeScalar,
         letterSpacingScalar: letterSpacingScalar ?? this.letterSpacingScalar,
         wordSpacingScalar: wordSpacingScalar ?? this.wordSpacingScalar,
