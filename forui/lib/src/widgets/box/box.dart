f8179314541400e8865c642a9b1607b2ce344be6
Joe Kawai <kawaijoe@users.noreply.github.com>
2024-05-17 16:08:08 +0800

    Card widget (#15)

--- forui/lib/src/widgets/box/box.dart
+++ forui/lib/src/widgets/box/box.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 import 'package:forui/forui.dart';
 
+part 'box_style.dart';
+
 /// A sample widget that demonstrates Forui's architecture.
 class FBox extends StatelessWidget {
   /// The text.
---
+++ forui/lib/src/widgets/card/card_content.dart
@@ -0,0 +1,40 @@
+part of 'card.dart';
+
+/// Represents a content for [FCard].
+final class FCardContent extends StatelessWidget {
+  /// The title.
+  final String? title;
+
+  /// The subtitle.
+  final String? subtitle;
+
+  /// The child.
+  final Widget? child;
+
+  /// The style.
+  final FCardContentStyle? style;
+
+  /// Creates a [FCardContent].
+  const FCardContent({this.title, this.subtitle, this.child, this.style, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final style = this.style ?? FTheme.of(context).widgets.card.content;
+
+    return Padding(
+      padding: style.padding,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          if (title != null) Text(title!, style: style.title),
+          if (subtitle != null) Text(subtitle!, style: style.subtitle),
+          if (child != null)
+            Padding(
+              padding: (title == null && subtitle == null) ? const EdgeInsets.only(top: 4) : const EdgeInsets.only(top: 10),
+              child: child!,
+            ),
+        ],
+      ),
+    );
+  }
+}
