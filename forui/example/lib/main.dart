3e6739687078147b72d530d81457d5600f7978b8
Matthias Ngeo <matthiasngeo@gmail.com>
2024-06-13 20:52:25 +0800

    Prototype dialog

--- forui/example/lib/main.dart
+++ forui/example/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 import 'package:forui/forui.dart';
@@ -17,10 +19,14 @@ class Application extends StatelessWidget {
           data: FThemes.zinc.light,
           child: Scaffold(
             backgroundColor: FThemes.zinc.light.colorScheme.background,
-            body: const Column(
+            body: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                ExampleWidget(),
+                const ExampleWidget(),
+                FButton(
+                  text: 'Hi',
+                  onPress: () {},
+                ),
               ],
             ),
           ),
@@ -46,10 +52,148 @@ class _ExampleWidgetState extends State<ExampleWidget> {
     textController = TextEditingController()..addListener(() {});
   }
 
+  Widget _dialog(BuildContext context) {
+    final style = context.theme.cardStyle;
+    final contentStyle = style.content;
+    final typography = context.theme.typography;
+    final effectivePadding =
+        MediaQuery.viewInsetsOf(context) + const EdgeInsets.symmetric(horizontal: 40.0, vertical: 24.0);
+
+    final button = Expanded(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 7.0),
+        child: FButton(design: FButtonVariant.outlined, text: 'Cancel', onPress: () {}),
+      ),
+    );
+
+    final child = Align(
+      child: DefaultTextStyle(
+        style: context.theme.typography.toTextStyle(
+          fontSize: typography.base,
+          color: context.theme.colorScheme.foreground,
+        ),
+        child: ConstrainedBox(
+            constraints: const BoxConstraints(minWidth: 280.0),
+            child: DecoratedBox(
+              decoration: style.decoration,
+              child: Padding(
+                padding: style.content.padding,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text('Are you absolutely sure?', style: contentStyle.title.scale(typography)),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                        'This action cannot be undone. This will permanently delete your account and remove your data from our servers.',
+                        style: contentStyle.subtitle.scale(typography), textAlign: TextAlign.center,),
+                    SizedBox(
+                      height: 18,
+                    ),
+                    LayoutBuilder(
+                      builder: (context, constraints) {
+                        // Get the width of the screen
+                        final screenWidth = MediaQuery.sizeOf(context).width;
+
+                        // Calculate the combined width of the buttons
+                        final buttonWidth = constraints.maxWidth;
+
+                        print(screenWidth);
+                        print(buttonWidth);
+
+                        // If the combined width of the buttons exceeds the screen width, place them in a Column
+                        if (true) {
+                          return Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.symmetric(vertical: 7.0),
+                                child: FButton(text: 'Continue', onPress: () {}),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.symmetric(vertical: 7.0),
+                                child: FButton(design: FButtonVariant.outlined, text: 'Cancel', onPress: () {}),
+                              ),
+                            ],
+                          );
+                        } else {
+                          // Otherwise, place them in a Row
+                          return Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Expanded(
+                                child: Padding(
+                                  padding: const EdgeInsets.symmetric(horizontal: 7.0),
+                                  child: FButton(design: FButtonVariant.outlined, text: 'Cancel', onPress: () {}),
+                                ),
+                              ),
+                              Expanded(
+                                child: Padding(
+                                  padding: const EdgeInsets.symmetric(horizontal: 7.0),
+                                  child: FButton(text: 'Continue', onPress: () {}),
+                                ),
+                              ),
+                            ],
+                          );
+                        }
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            )),
+      ),
+    );
+
+    return AnimatedPadding(
+      padding: effectivePadding,
+      duration: const Duration(milliseconds: 100),
+      curve: Curves.decelerate,
+      child: MediaQuery.removeViewInsets(
+        removeLeft: true,
+        removeTop: true,
+        removeRight: true,
+        removeBottom: true,
+        context: context,
+        child: child,
+      ),
+    );
+  }
+
   @override
-  Widget build(BuildContext context) => Padding(
-    padding: const EdgeInsets.all(8.0),
-    child: Column(
+  Widget build(BuildContext context) {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      showAdaptiveDialog(
+        context: context,
+        builder: (context) {
+          final style = context.theme.cardStyle;
+          final contentStyle = style.content;
+          final typography = context.theme.typography;
+
+          return _dialog(context);
+
+          // return AlertDialog()
+          // return Dialog(child: Column(
+          //   crossAxisAlignment: CrossAxisAlignment.start,
+          //   children: [
+          //     Text('Are you absolutely sure?', style: contentStyle.title.scale(typography)),
+          //     Text('This action cannot be undone. This will permanently delete your account and remove your data from our servers.', style: contentStyle.subtitle.scale(typography)),
+          //     Row(
+          //       children: [
+          //         FButton(design: FButtonVariant.secondary, text: 'Cancel', onPress: () {}),
+          //         FButton(text: 'Continue', onPress: () {}),
+          //       ],
+          //     )
+          //   ],
+          // ),);
+        },
+      );
+    });
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
         children: [
           // FCard(title: 'Email'),
           FTextField(
@@ -71,7 +215,8 @@ class _ExampleWidgetState extends State<ExampleWidget> {
           // ),
         ],
       ),
-  );
+    );
+  }
 
   @override
   void dispose() {
