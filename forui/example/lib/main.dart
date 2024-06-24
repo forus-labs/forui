e62f6827bf35375f5f9e99f8227e28357fce6ab4
Joe Kawai <kawaijoe@users.noreply.github.com>
2024-06-24 13:23:46 +0800

    Core documentation (#50)
    * Complete getting started
    
    * Complete getting started
    
    * Complete docs

--- forui/example/lib/main.dart
+++ forui/example/lib/main.dart
@@ -28,10 +28,6 @@ class Application extends StatelessWidget {
       );
 }
 
-
-
-
-
 class ExampleWidget extends StatefulWidget {
   const ExampleWidget({super.key});
 
@@ -40,37 +36,16 @@ class ExampleWidget extends StatefulWidget {
 }
 
 class _ExampleWidgetState extends State<ExampleWidget> {
-
   @override
   void initState() {
     super.initState();
   }
 
   @override
-  Widget build(BuildContext context) => Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          FButton(
-            style: FButtonStyle.destructive,
-            label: 'Delete?',
-            onPress: () => showAdaptiveDialog(
-              context: context,
-              builder: (context) => FDialog(
-                direction: Axis.horizontal,
-                title: 'Are you absolutely sure?',
-                body: 'This action cannot be undone. This will permanently delete your account and remove your data from our servers.',
-                actions: [
-                  FButton(style: FButtonStyle.outline, label: 'Cancel', onPress: () {
-                    Navigator.of(context).pop();
-                  }),
-                  FButton(label: 'Continue', onPress: () {}),
-                ],
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
+  Widget build(BuildContext context) => const Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+        ),
+      );
 }
