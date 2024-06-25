d12868b79677103aa1554ac0e5cd4a47c06e2401
Matthias Ngeo <matthiasngeo@gmail.com>
2024-06-25 16:50:09 +0800

    Add examples

--- docs/components/widget.tsx
+++ docs/components/widget.tsx
@@ -1,22 +1,18 @@
 import {useTheme} from "nextra-theme-docs";
 
 interface Props {
-    name: string;
-    variant?: string;
     query: Record<string, string>;
 }
 
-export function Widget({name, variant = 'default', query}: Props) {
+export function Widget({query}: Props) {
     const {resolvedTheme} = useTheme();
     query['theme'] = resolvedTheme;
 
     const url = process.env['NEXT_PUBLIC_DEMO_URL'];
 
-    return (
-        <iframe
-            src={`${url}/${name}/${variant}?${new URLSearchParams(query).toString()}`}
-            width={0}
-            height={0}
-        />
-    );
+    return <iframe
+        src={`${url}?${new URLSearchParams(query).toString()}`}
+        width={0}
+        height={0}
+    />;
 }
