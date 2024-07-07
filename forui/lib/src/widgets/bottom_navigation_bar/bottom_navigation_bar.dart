4ac489ea742832527b8edaafb1fd0834f4e63da6
Joe <kawaijoe@gmail.com>
2024-07-07 22:45:46 +0800

    Boilerplate for bottom navigation bar

---
+++ forui/lib/src/widgets/bottom_navigation_bar/bottom_navigation_bar.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/widgets.dart';
+
+part 'bottom_navigation_bar_item.dart';
+
+class FBottomNavigationBar extends StatelessWidget {
+  const FBottomNavigationBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+class FBottomNavigationBarStyle {}
