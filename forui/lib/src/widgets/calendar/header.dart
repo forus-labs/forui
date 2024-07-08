7ff9929b760f456daa073736edf7b08b4a0f1ed4
Matthias Ngeo <matthiasngeo@gmail.com>
2024-07-08 22:15:17 +0800

    Add calendar header

--- forui/lib/src/widgets/calendar/header.dart
+++ forui/lib/src/widgets/calendar/header.dart
@@ -1,2 +1,131 @@
 part of 'calendar.dart';
 
+@internal
+class Header extends StatelessWidget {
+  final FCalendarHeaderStyle style;
+  final DateTime month;
+  final VoidCallback? onPrevious;
+  final VoidCallback? onNext;
+
+  const Header({
+    required this.style,
+    required this.month,
+    required this.onPrevious,
+    required this.onNext,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final buttonStyle = context.theme.buttonStyles.outline;
+    final effectiveButtonStyle = buttonStyle.copyWith(
+      enabledBoxDecoration: buttonStyle.enabledBoxDecoration.copyWith(borderRadius: BorderRadius.circular(4)),
+      disabledBoxDecoration: buttonStyle.disabledBoxDecoration.copyWith(borderRadius: BorderRadius.circular(4)),
+    );
+
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 5),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 7),
+            child: FButton.raw(
+              style: effectiveButtonStyle,
+              onPress: onPrevious,
+              child: Padding(
+                padding: const EdgeInsets.all(5),
+                child: FAssets.icons.chevronLeft(
+                  height: 16,
+                  colorFilter: ColorFilter.mode(
+                    style.iconColor,
+                    BlendMode.srcIn,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Text('July ${month.year}', style: style.headerTextStyle), // TODO: Localization
+          Padding(
+            padding: const EdgeInsets.only(right: 7),
+            child: FButton.raw(
+              style: effectiveButtonStyle,
+              onPress: onNext,
+              child: Padding(
+                padding: const EdgeInsets.all(5),
+                child: FAssets.icons.chevronRight(
+                  height: 16,
+                  colorFilter: ColorFilter.mode(
+                    style.iconColor,
+                    BlendMode.srcIn,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties
+      ..add(DiagnosticsProperty('style', style))
+      ..add(DiagnosticsProperty('month', month))
+      ..add(DiagnosticsProperty('onPrevious', onPrevious))
+      ..add(DiagnosticsProperty('onNext', onNext));
+  }
+}
+
+/// The calendar header's style.
+final class FCalendarHeaderStyle {
+  /// The header's text style.
+  final TextStyle headerTextStyle;
+
+  /// The unfocused header icons' color.
+  final Color iconColor;
+
+  /// Creates a [FCalendarHeaderStyle].
+  FCalendarHeaderStyle({
+    required this.headerTextStyle,
+    required this.iconColor,
+  });
+
+  /// Creates a copy of this but with the given fields replaced with the new values.
+  ///
+  /// ```dart
+  /// final style = FCalendarHeaderStyle(
+  ///   headerTextStyle: ...,
+  ///   iconColor:...,
+  ///   // Other arguments omitted for brevity.
+  /// );
+  ///
+  /// final copy = style.copyWith(
+  ///   iconColor: ...,
+  /// );
+  ///
+  /// print(style.headerTextStyle == copy.headerTextStyle); // true
+  /// print(style.iconColor == copy.iconColor); // false
+  /// ```
+  FCalendarHeaderStyle copyWith({
+    TextStyle? headerTextStyle,
+    Color? iconColor,
+  }) =>
+      FCalendarHeaderStyle(
+        headerTextStyle: headerTextStyle ?? this.headerTextStyle,
+        iconColor: iconColor ?? this.iconColor,
+      );
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is FCalendarHeaderStyle &&
+          runtimeType == other.runtimeType &&
+          headerTextStyle == other.headerTextStyle &&
+          iconColor == other.iconColor;
+
+  @override
+  int get hashCode => headerTextStyle.hashCode ^ iconColor.hashCode;
+}
