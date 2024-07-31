9e25b539dd957a252f3f557600210be78eb456c1
Matthias <matthiasngeo@gmail.com>
2024-07-31 22:53:25 +0800

    Fix failing tests

--- forui/test/src/widgets/resizable/resizable_test.dart
+++ forui/test/src/widgets/resizable/resizable_test.dart
@@ -1,29 +1,27 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' hide VerticalDivider;
 
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:forui/forui.dart';
+import 'package:forui/src/widgets/resizable/divider.dart';
 
 void main() {
   final top = FResizableRegion(
     initialExtent: 30,
-    minExtent: 10,
-    builder: (context, snapshot, child) => const SizedBox(),
+    minExtent: 20,
+    builder: (context, snapshot, child) => const Align(child: Text('A')),
   );
 
   final bottom = FResizableRegion(
     initialExtent: 70,
-    minExtent: 10,
-    builder: (context, snapshot, child) => const SizedBox(),
+    minExtent: 20,
+    builder: (context, snapshot, child) => const Align(child: Text('B')),
   );
 
   for (final (index, constructor) in [
     () => FResizable(crossAxisExtent: 0, axis: Axis.vertical, children: [top, bottom]),
   ].indexed) {
-    test(
-      '[$index] constructor throws error',
-      () => expect(constructor, throwsAssertionError),
-    );
+    test('[$index] constructor throws error', () => expect(constructor, throwsAssertionError));
   }
 
   testWidgets('vertical drag downwards', (tester) async {
@@ -41,20 +39,14 @@ void main() {
     );
 
     await tester.timedDrag(
-      find.byType(GestureDetector).at(1),
+      find.byType(VerticalDivider),
       const Offset(0, 100),
       const Duration(seconds: 1),
     );
     await tester.pumpAndSettle();
 
-    expect(
-      tester.getSize(find.byType(FResizableRegion).first),
-      const Size(50, 80),
-    );
-    expect(
-      tester.getSize(find.byType(FResizableRegion).last),
-      const Size(50, 20),
-    );
+    expect(tester.getSize(find.byType(FResizableRegion).first), const Size(50, 80));
+    expect(tester.getSize(find.byType(FResizableRegion).last), const Size(50, 20));
   });
 
   testWidgets('vertical drag upwards', (tester) async {
@@ -72,7 +64,7 @@ void main() {
     );
 
     await tester.timedDrag(
-      find.byType(GestureDetector).at(1),
+      find.byType(VerticalDivider),
       const Offset(0, -100),
       const Duration(seconds: 1),
     );
@@ -103,7 +95,7 @@ void main() {
     );
 
     await tester.timedDrag(
-      find.byType(GestureDetector).at(1),
+      find.byType(HorizontalDivider),
       const Offset(100, 0),
       const Duration(seconds: 1),
     );
@@ -134,7 +126,7 @@ void main() {
     );
 
     await tester.timedDrag(
-      find.byType(GestureDetector).at(1),
+      find.byType(HorizontalDivider),
       const Offset(-100, 0),
       const Duration(seconds: 1),
     );
@@ -147,37 +139,6 @@ void main() {
     expect(
       tester.getSize(find.byType(FResizableRegion).last),
       const Size(80, 50),
-    );
-  });
-
-  testWidgets('horizontal drag right', (tester) async {
-    final horizontal = FResizable(
-      crossAxisExtent: 50,
-      axis: Axis.horizontal,
-      children: [top, bottom],
-    );
-
-    await tester.pumpWidget(
-      MaterialApp(
-        theme: ThemeData(useMaterial3: true),
-        home: Scaffold(body: Center(child: horizontal)),
-      ),
-    );
-
-    await tester.timedDrag(
-      find.byType(GestureDetector).at(1),
-      const Offset(100, 0),
-      const Duration(seconds: 1),
-    );
-    await tester.pumpAndSettle();
-
-    expect(
-      tester.getSize(find.byType(FResizableRegion).first),
-      const Size(80, 50),
-    );
-    expect(
-      tester.getSize(find.byType(FResizableRegion).last),
-      const Size(20, 50),
     );
   });
 }
