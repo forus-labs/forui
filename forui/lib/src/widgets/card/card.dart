3db5c58bbe9ff16b65df2129eb82d5443ec9009b
Daviiddoo <Daviiddoo@users.noreply.github.com>
2024-08-06 10:24:38 +0000

    Commit from GitHub Actions (Forui Presubmit)

--- forui/lib/src/widgets/card/card.dart
+++ forui/lib/src/widgets/card/card.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
-import 'package:forui/util.dart';
 
 import 'package:meta/meta.dart';
 
 import 'package:forui/forui.dart';
+import 'package:forui/util.dart';
 
 part 'card_content.dart';
 
