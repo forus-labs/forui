88ce93812d36396c294ac83718242536ddf789b7
Somye <mahajan.somye@gmail.com>
2024-08-11 09:46:08 +0530

    Renamed icon to child in FButton.icon(...)

--- forui/lib/src/widgets/button/button.dart
+++ forui/lib/src/widgets/button/button.dart
@@ -27,7 +27,8 @@ class FButton extends StatelessWidget {
   @useResult
   static _Data _of(BuildContext context) {
     final theme = context.dependOnInheritedWidgetOfExactType<_InheritedData>();
-    return theme?.data ?? (style: context.theme.buttonStyles.primary, enabled: true);
+    return theme?.data ??
+        (style: context.theme.buttonStyles.primary, enabled: true);
   }
 
   /// The style. Defaults to [FButtonStyle.primary].
@@ -99,17 +100,17 @@ class FButton extends StatelessWidget {
           label: label,
         );
 
-  /// Creates a [FButton] that contains only an [icon].
+  /// Creates a [FButton] that contains only an icon
   FButton.icon({
     required this.onPress,
-    required Widget icon,
+    required Widget child,
     this.style = Variant.outline,
     this.onLongPress,
     this.autofocus = false,
     this.focusNode,
     this.onFocusChange,
     super.key,
-  }) : child = _FButtonIconContent(icon: icon);
+  }) : child = _FButtonIconContent(child: child);
 
   /// Creates a [FButton] with custom content.
   const FButton.raw({
@@ -141,7 +142,8 @@ class FButton extends StatelessWidget {
       onPress: onPress,
       onLongPress: onLongPress,
       child: DecoratedBox(
-        decoration: enabled ? style.enabledBoxDecoration : style.disabledBoxDecoration,
+        decoration:
+            enabled ? style.enabledBoxDecoration : style.disabledBoxDecoration,
         child: _InheritedData(
           data: (style: style, enabled: enabled),
           child: child,
@@ -157,7 +159,8 @@ class FButton extends StatelessWidget {
       ..add(DiagnosticsProperty('style', style))
       ..add(DiagnosticsProperty('onPress', onPress))
       ..add(DiagnosticsProperty('onLongPress', onLongPress))
-      ..add(FlagProperty('autofocus', value: autofocus, defaultValue: false, ifTrue: 'autofocus'))
+      ..add(FlagProperty('autofocus',
+          value: autofocus, defaultValue: false, ifTrue: 'autofocus'))
       ..add(DiagnosticsProperty('focusNode', focusNode))
       ..add(DiagnosticsProperty('onFocusChange', onFocusChange))
       ..add(DiagnosticsProperty('builder', child));
@@ -245,7 +248,8 @@ class FButtonCustomStyle extends FButtonStyle with Diagnosticable {
   }) =>
       FButtonCustomStyle(
         enabledBoxDecoration: enabledBoxDecoration ?? this.enabledBoxDecoration,
-        disabledBoxDecoration: disabledBoxDecoration ?? this.disabledBoxDecoration,
+        disabledBoxDecoration:
+            disabledBoxDecoration ?? this.disabledBoxDecoration,
         content: content ?? this.content,
         icon: icon ?? this.icon,
       );
@@ -271,7 +275,11 @@ class FButtonCustomStyle extends FButtonStyle with Diagnosticable {
           icon == other.icon;
 
   @override
-  int get hashCode => enabledBoxDecoration.hashCode ^ disabledBoxDecoration.hashCode ^ content.hashCode ^ icon.hashCode;
+  int get hashCode =>
+      enabledBoxDecoration.hashCode ^
+      disabledBoxDecoration.hashCode ^
+      content.hashCode ^
+      icon.hashCode;
 }
 
 typedef _Data = ({FButtonCustomStyle style, bool enabled});
