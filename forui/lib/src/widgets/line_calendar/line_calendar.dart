ffb1889ea1e2efede0a31467c2fdb94c1585b681
Daviiddoo <Davidchew12345@gmail.com>
2024-08-19 03:02:50 +0800

    styling done

--- forui/lib/src/widgets/line_calendar/line_calendar.dart
+++ forui/lib/src/widgets/line_calendar/line_calendar.dart
@@ -2,13 +2,16 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 import 'package:forui/forui.dart';
 import 'package:forui/src/foundation/tappable.dart';
+import 'package:meta/meta.dart';
 
 import 'package:sugar/sugar.dart';
 
 const _days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
 final _epoch = LocalDate(2000);
 
+/// A calendar that can be scrolled horizontally.
 class FLineCalendar extends StatefulWidget {
+  /// The style. Defaults to [FThemeData.lineCalendarStyle].
   final FLineCalendarStyle? style;
 
   /// The currently selected [LocalDate].
@@ -32,40 +35,59 @@ class FLineCalendar extends StatefulWidget {
 
   @override
   State<FLineCalendar> createState() => _FLineCalendarState();
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties
+      ..add(DiagnosticsProperty<FLineCalendarStyle?>('style', style))
+      ..add(DiagnosticsProperty<ValueNotifier<LocalDate>>('selected', selected))
+      ..add(DiagnosticsProperty<LocalDate>('epoch', epoch))
+      ..add(DiagnosticsProperty<LocalDate>('today', today));
+  }
 }
 
 class _FLineCalendarState extends State<FLineCalendar> {
-  late final ScrollController _controller;
-  late final double _width;
+  late ScrollController _controller = ScrollController();
+  late double _width;
 
   @override
   void initState() {
     widget.selected.addListener(() => setState(() {}));
-    final mediaQuery = (context.getElementForInheritedWidgetOfExactType<MediaQuery>()!.widget as MediaQuery).data;
-    _width = mediaQuery.size.width / 5;
-    final offset = (widget.selected.value.difference(widget.epoch).inDays - 2) * _width;
-    _controller = ScrollController(initialScrollOffset: offset);
     super.initState();
   }
 
   @override
-  Widget build(BuildContext context) => SizedBox(
-        height: 85,
-        child: ListView.builder(
-          controller: _controller,
-          scrollDirection: Axis.horizontal,
-          padding: EdgeInsets.zero,
-          itemExtent: _width,
-          itemBuilder: (context, index) => Container(
-            padding: EdgeInsets.symmetric(horizontal: (_width - 55) / 2),
-            child: _Tile(
-              style: widget.style,
-              selected: widget.selected,
-              date: widget.epoch.add(Duration(days: index)),
-              today: widget.today,
+  Widget build(BuildContext context) => LayoutBuilder(
+        builder: (context, constraints) {
+          if (constraints.maxWidth < 450) {
+            _width = constraints.maxWidth / 5;
+          } else if (constraints.maxWidth >= 450 && constraints.maxWidth <= 650) {
+            _width = constraints.maxWidth / 7;
+          } else {
+            _width = constraints.maxWidth / 10;
+          }
+          final offset = (widget.selected.value.difference(widget.epoch).inDays - 2) * _width;
+          _controller = ScrollController(initialScrollOffset: offset);
+          return SizedBox(
+            height: 70,
+            child: ListView.builder(
+              controller: _controller,
+              scrollDirection: Axis.horizontal,
+              padding: EdgeInsets.zero,
+              itemExtent: _width,
+              itemBuilder: (context, index) => Container(
+                padding: EdgeInsets.symmetric(horizontal: (_width - 55) / 2),
+                child: _Tile(
+                  style: widget.style,
+                  selected: widget.selected,
+                  date: widget.epoch.add(Duration(days: index)),
+                  today: widget.today,
+                ),
+              ),
             ),
-          ),
-        ),
+          );
+        },
       );
 
   @override
@@ -84,6 +106,16 @@ class _Tile extends StatelessWidget {
   const _Tile({required this.selected, required this.date, required LocalDate today, this.style})
       : underline = date == today;
 
+  TextStyle _style(BuildContext context, bool selected) {
+    final style = this.style ?? context.theme.lineCalendarStyle;
+    return switch ((selected, underline)) {
+      (true, true) => style.selectedTextStyle.copyWith(decoration: TextDecoration.underline),
+      (true, false) => style.selectedTextStyle,
+      (false, true) => style.unselectedTextStyle.copyWith(decoration: TextDecoration.underline),
+      (false, false) => style.unselectedTextStyle,
+    };
+  }
+
   @override
   Widget build(BuildContext context) {
     final style = this.style ?? context.theme.lineCalendarStyle;
@@ -91,104 +123,133 @@ class _Tile extends StatelessWidget {
 
     return FTappable.animated(
       onPress: () => this.selected.value = date,
-      child: Container(
-        alignment: Alignment.center,
-        width: 55,
-        height: 85,
-        padding: const EdgeInsets.all(8),
-        decoration: selected ? style.selectedDecoration : null,
+      child: DecoratedBox(
+        decoration: selected ? style.selectedDecoration : style.unselectedDecoration,
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            _Date(
-              date: date,
-              selected: selected,
-              underline: underline,
-              style: style,
+            Text(
+              date.day.toString(),
+              style: selected ? style.selectedTextStyle : style.unselectedTextStyle,
             ),
-            const SizedBox(height: 5),
+            const SizedBox(height: 4),
             Text(
               _days[date.weekday - 1],
-              style: selected ? style.selectedTextStyle : style.unselectedTextStyle,
+              style: _style(context, selected),
             ),
           ],
         ),
       ),
     );
   }
-}
-
-class _Date extends StatelessWidget {
-  final FLineCalendarStyle? style;
-  final LocalDate date;
-  final bool selected;
-  final bool underline;
-
-  const _Date({required this.date, required this.selected, required this.underline, this.style});
 
   @override
-  Widget build(BuildContext context) {
-    final style = this.style ?? context.theme.lineCalendarStyle;
-    return Container(
-      alignment: Alignment.center,
-      height: 40,
-      width: 40,
-      decoration: style.dateDecoration,
-      child: Text(
-        date.day.toString(),
-        style: selected ? style.selectedTextStyle : style.unselectedTextStyle,
-      ),
-    );
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties
+      ..add(DiagnosticsProperty('style', style))
+      ..add(DiagnosticsProperty<ValueNotifier<LocalDate>>('selected', selected))
+      ..add(DiagnosticsProperty('date', date))
+      ..add(DiagnosticsProperty('underline', underline));
   }
 }
 
 /// [FAvatar]'s style.
 final class FLineCalendarStyle with Diagnosticable {
-  /// The fallback's background color.
-  final Color backgroundColor;
-
-  /// The fallback's color.
-  final Color foregroundColor;
-
-  final BoxDecoration dateDecoration;
-
+  /// The box decoration for a selected date.
   final BoxDecoration selectedDecoration;
+
+  /// The box decoration for an unselected date.
+  final BoxDecoration unselectedDecoration;
 
   /// The text style for the selected date.
   final TextStyle selectedTextStyle;
 
-  /// The text style for the selected date.
+  /// The text style for an unselected date.
   final TextStyle unselectedTextStyle;
 
   /// Creates a [FLineCalendarStyle].
   const FLineCalendarStyle({
-    required this.backgroundColor,
-    required this.foregroundColor,
-    required this.dateDecoration,
+    required this.unselectedDecoration,
     required this.selectedDecoration,
     required this.selectedTextStyle,
     required this.unselectedTextStyle,
   });
 
   /// Creates a [FCardStyle] that inherits its properties from [colorScheme] and [typography].
-  FLineCalendarStyle.inherit(
-      {required FColorScheme colorScheme, required FTypography typography, required FStyle style})
-      : backgroundColor = colorScheme.muted,
-        foregroundColor = colorScheme.mutedForeground,
-        dateDecoration = BoxDecoration(
-          color: colorScheme.foreground,
+  FLineCalendarStyle.inherit({
+    required FColorScheme colorScheme,
+    required FTypography typography,
+    required FStyle style,
+  })  : selectedDecoration = BoxDecoration(
+          color: colorScheme.primary,
           borderRadius: style.borderRadius,
         ),
-        selectedDecoration = BoxDecoration(
-          color: colorScheme.primary,
+        unselectedDecoration = BoxDecoration(
+          color: colorScheme.background,
           borderRadius: style.borderRadius,
           border: Border.all(color: colorScheme.border),
         ),
         selectedTextStyle = typography.base.copyWith(
-          color: colorScheme.mutedForeground,
+          color: colorScheme.primaryForeground,
           height: 0,
         ),
         unselectedTextStyle = typography.base.copyWith(
-          color: colorScheme.mutedForeground,
+          color: colorScheme.primary,
           height: 0,
         );
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties
+      ..add(DiagnosticsProperty('selectedDecoration', selectedDecoration))
+      ..add(DiagnosticsProperty('unselectedDecoration', unselectedDecoration))
+      ..add(DiagnosticsProperty('selectedTextStyle', selectedTextStyle))
+      ..add(DiagnosticsProperty('unselectedTextStyle', unselectedTextStyle));
+  }
+
+  /// Returns a copy of this [FLineCalendarStyle] with the given properties replaced.
+  ///
+  /// ```dart
+  /// final style = FLineCalendarStyle(
+  ///   selectedDecoration: ...,
+  ///   unselectedDecoration: ...,
+  /// );
+  ///
+  /// final copy = style.copyWith(unselectedDecoration: ...);
+  ///
+  /// print(style.selectedDecoration == copy.selectedDecoration); // true
+  /// print(style.unselectedDecoration == copy.unselectedDecoration); // false
+  /// ```
+  @useResult
+  FLineCalendarStyle copyWith({
+    BoxDecoration? selectedDecoration,
+    BoxDecoration? unselectedDecoration,
+    TextStyle? selectedTextStyle,
+    TextStyle? unselectedTextStyle,
+  }) =>
+      FLineCalendarStyle(
+        selectedDecoration: selectedDecoration ?? this.selectedDecoration,
+        unselectedDecoration: unselectedDecoration ?? this.unselectedDecoration,
+        selectedTextStyle: selectedTextStyle ?? this.selectedTextStyle,
+        unselectedTextStyle: unselectedTextStyle ?? this.unselectedTextStyle,
+      );
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is FLineCalendarStyle &&
+          runtimeType == other.runtimeType &&
+          selectedDecoration == other.selectedDecoration &&
+          unselectedDecoration == other.unselectedDecoration &&
+          selectedTextStyle == other.selectedTextStyle &&
+          unselectedTextStyle == other.unselectedTextStyle;
+
+  @override
+  int get hashCode =>
+      selectedDecoration.hashCode ^
+      unselectedDecoration.hashCode ^
+      selectedTextStyle.hashCode ^
+      unselectedTextStyle.hashCode;
 }
