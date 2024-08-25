969bf297251bec5f61b361335a7d82ec87addb37
Matthias Ngeo <matthiasngeo@gmail.com>
2024-08-25 20:43:55 +0800

    Add documentation and portal shift tests

--- forui/lib/src/foundation/portal/portal.dart
+++ forui/lib/src/foundation/portal/portal.dart
@@ -3,26 +3,60 @@ import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 import 'package:forui/forui.dart';
 
+/// A portal renders a follower widget that "floats" on top of a target widget.
+///
+/// Similar to an [OverlayPortal], it requires an [Overlay] ancestor. Unlike an [OverlayPortal], the follower is aligned
+/// relative to the target.
+///
+/// See:
+/// * [FPortalFollowerShift] for the various follower shifting strategies.
+/// * [OverlayPortalController] for controlling the follower's visibility.
+/// * [OverlayPortal] for the underlying widget.
 class FPortal extends StatefulWidget {
+  /// The controller that shows and hides the follower. It initially hides the follower.
   final OverlayPortalController controller;
-  final Alignment childAnchor;
+
+  /// The anchor of the follower to which the [childAnchor] is aligned to. Defaults to [Alignment.topCenter].
   final Alignment followerAnchor;
-  final Offset Function(Size, FPortalShiftTarget, FPortalShiftFollower) shift;
+
+  /// The anchor of the target to which the [followerAnchor] is aligned to. Defaults to [Alignment.bottomCenter].
+  final Alignment childAnchor;
+
+  /// The shifting strategy used to shift a follower when it overflows out of the viewport. Defaults to
+  /// [FPortalFollowerShift.flip].
+  final Offset Function(Size, FPortalTarget, FPortalFollower) shift;
+
+  /// The follower.
   final WidgetBuilder follower;
+
+  /// The target.
   final Widget child;
 
-  FPortal({
+  /// Creates a portal.
+  const FPortal({
     required this.controller,
     required this.follower,
     required this.child,
     this.followerAnchor = Alignment.topCenter,
     this.childAnchor = Alignment.bottomCenter,
-    this.shift = FPortalShift.flip,
+    this.shift = FPortalFollowerShift.flip,
     super.key,
   });
 
   @override
   State<FPortal> createState() => _State();
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties
+      ..add(DiagnosticsProperty('controller', controller))
+      ..add(DiagnosticsProperty('followerAnchor', followerAnchor))
+      ..add(DiagnosticsProperty('childAnchor', childAnchor))
+      ..add(DiagnosticsProperty('shift', shift))
+      ..add(DiagnosticsProperty('follower', follower))
+      ..add(DiagnosticsProperty('child', child));
+  }
 }
 
 class _State extends State<FPortal> {
@@ -58,14 +92,14 @@ class _Alignment extends SingleChildRenderObjectWidget {
   final LayerLink _link;
   final Alignment _targetAnchor;
   final Alignment _followerAnchor;
-  final Offset Function(Size, FPortalShiftTarget, FPortalShiftFollower) _shift;
+  final Offset Function(Size, FPortalTarget, FPortalFollower) _shift;
 
   const _Alignment({
     required Widget child,
     required LayerLink link,
     required Alignment targetAnchor,
     required Alignment followerAnchor,
-    required Offset Function(Size, FPortalShiftTarget, FPortalShiftFollower) shift,
+    required Offset Function(Size, FPortalTarget, FPortalFollower) shift,
   })  : _link = link,
         _targetAnchor = targetAnchor,
         _followerAnchor = followerAnchor,
@@ -94,13 +128,13 @@ class _RenderBox extends RenderBox with RenderObjectWithChildMixin<RenderBox> {
   LayerLink _link;
   Alignment _targetAnchor;
   Alignment _followerAnchor;
-  Offset Function(Size, FPortalShiftTarget, FPortalShiftFollower) _shift;
+  Offset Function(Size, FPortalTarget, FPortalFollower) _shift;
 
   _RenderBox({
     required LayerLink link,
     required Alignment targetAnchor,
     required Alignment followerAnchor,
-    required Offset Function(Size, FPortalShiftTarget, FPortalShiftFollower) shift,
+    required Offset Function(Size, FPortalTarget, FPortalFollower) shift,
   })  : _link = link,
         _targetAnchor = targetAnchor,
         _followerAnchor = followerAnchor,
@@ -120,7 +154,11 @@ class _RenderBox extends RenderBox with RenderObjectWithChildMixin<RenderBox> {
     if ((child, link.leader?.offset, link.leaderSize) case (final child?, final offset?, final leaderSize?)) {
       context.paintChild(
         child,
-        _shift(size, (offset: offset, size: leaderSize, anchor: targetAnchor), (box: child, anchor: followerAnchor)),
+        _shift(
+          size,
+          (offset: offset, size: leaderSize, anchor: targetAnchor),
+          (size: child.size, anchor: followerAnchor),
+        ),
       );
     }
   }
@@ -132,7 +170,7 @@ class _RenderBox extends RenderBox with RenderObjectWithChildMixin<RenderBox> {
         offset: _shift(
           size,
           (offset: offset, size: leaderSize, anchor: targetAnchor),
-          (box: child, anchor: followerAnchor),
+          (size: child.size, anchor: followerAnchor),
         ),
         position: position,
         hitTest: (result, transformed) => child.hitTest(result, position: transformed),
@@ -147,7 +185,8 @@ class _RenderBox extends RenderBox with RenderObjectWithChildMixin<RenderBox> {
     properties
       ..add(DiagnosticsProperty('link', link))
       ..add(DiagnosticsProperty('targetAnchor', targetAnchor))
-      ..add(DiagnosticsProperty('followerAnchor', followerAnchor));
+      ..add(DiagnosticsProperty('followerAnchor', followerAnchor))
+      ..add(DiagnosticsProperty('shift', shift));
   }
 
   LayerLink get link => _link;
@@ -171,9 +210,9 @@ class _RenderBox extends RenderBox with RenderObjectWithChildMixin<RenderBox> {
     markNeedsPaint();
   }
 
-  Offset Function(Size, FPortalShiftTarget, FPortalShiftFollower) get shift => _shift;
+  Offset Function(Size, FPortalTarget, FPortalFollower) get shift => _shift;
 
-  set shift(Offset Function(Size, FPortalShiftTarget, FPortalShiftFollower) value) {
+  set shift(Offset Function(Size, FPortalTarget, FPortalFollower) value) {
     _shift = value;
     markNeedsPaint();
   }
--- forui/lib/src/foundation/portal/portal_shift.dart
+++ forui/lib/src/foundation/portal/portal_shift.dart
@@ -2,28 +2,33 @@ import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 import 'package:meta/meta.dart';
 
-typedef FPortalShiftTarget = ({Offset offset, Size size, Alignment anchor});
+/// A portal's target.
+typedef FPortalTarget = ({Offset offset, Size size, Alignment anchor});
 
-typedef FPortalShiftFollower = ({RenderBox box, Alignment anchor});
+/// A portal's follower.
+typedef FPortalFollower = ({Size size, Alignment anchor});
 
-extension FPortalShift on Never {
-  static Offset flip(Size view, FPortalShiftTarget target, FPortalShiftFollower follower) {
+/// Provides various follower shifting strategies for when a follower overflows out of the viewport.
+extension FPortalFollowerShift on Never {
+
+  /// Flips the follower to the opposite side of the target if it does not cause the follower to overflow out of the
+  /// viewport. Otherwise shifts the follower along the target's edge.
+  static Offset flip(Size view, FPortalTarget target, FPortalFollower follower) {
     var anchor = none(view, target, follower).translate(target.offset.dx, target.offset.dy);
 
     final viewBox = Offset.zero & view;
-    final followerBox = anchor & follower.box.size;
+    final followerBox = anchor & follower.size;
 
     switch ((viewBox, followerBox)) {
       case _ when followerBox.left < viewBox.left:
         final flipped = _flip(target, follower, x: true);
-        if ((flipped & follower.box.size).right <= viewBox.right) {
+        if ((flipped & follower.size).right <= viewBox.right) {
           anchor = flipped;
         }
 
       case _ when viewBox.right < followerBox.right:
         final flipped = _flip(target, follower, x: true);
-        if (viewBox.left <= (flipped & follower.box.size).left) {
-          print('flip');
+        if (viewBox.left <= (flipped & follower.size).left) {
           anchor = flipped;
         }
     }
@@ -31,35 +36,36 @@ extension FPortalShift on Never {
     switch ((viewBox, followerBox)) {
       case _ when followerBox.top < viewBox.top:
         final flipped = _flip(target, follower, x: false);
-        if ((flipped & follower.box.size).bottom <= viewBox.bottom) {
+        if ((flipped & follower.size).bottom <= viewBox.bottom) {
           anchor = flipped;
         }
 
       case _ when viewBox.bottom < followerBox.bottom:
         final flipped = _flip(target, follower, x: false);
-        if (viewBox.top <= (flipped & follower.box.size).top) {
+        if (viewBox.top <= (flipped & follower.size).top) {
           anchor = flipped;
         }
     }
 
-    final foo = _along(anchor, viewBox, anchor & follower.box.size);
+    final foo = _along(anchor, viewBox, anchor & follower.size);
     return foo.translate(-target.offset.dx, -target.offset.dy);
   }
 
-  static Offset _flip(FPortalShiftTarget target, FPortalShiftFollower follower, {required bool x}) {
+  static Offset _flip(FPortalTarget target, FPortalFollower follower, {required bool x}) {
     final targetAnchor = x ? target.anchor.flipX() : target.anchor.flipY();
     final followerAnchor = x ? follower.anchor.flipX() : follower.anchor.flipY();
 
-    final anchor = targetAnchor.relative(to: target.size) - followerAnchor.relative(to: follower.box.size);
+    final anchor = targetAnchor.relative(to: target.size) - followerAnchor.relative(to: follower.size);
 
     return anchor.translate(target.offset.dx, target.offset.dy);
   }
 
-  static Offset along(Size view, FPortalShiftTarget target, FPortalShiftFollower follower) {
+  /// Shifts the follower along the target's edge if the follower overflows out of the viewport.
+  static Offset along(Size view, FPortalTarget target, FPortalFollower follower) {
     final anchor = none(view, target, follower).translate(target.offset.dx, target.offset.dy);
 
     final viewBox = Offset.zero & view;
-    final followerBox = anchor & follower.box.size;
+    final followerBox = anchor & follower.size;
 
     return _along(anchor, viewBox, followerBox).translate(-target.offset.dx, -target.offset.dy);
   }
@@ -80,9 +86,10 @@ extension FPortalShift on Never {
     return anchor;
   }
 
-  static Offset none(Size view, FPortalShiftTarget target, FPortalShiftFollower follower) {
+  /// Does not perform any shifting if the follower overflows out of the viewport.
+  static Offset none(Size view, FPortalTarget target, FPortalFollower follower) {
     final targetAnchor = target.anchor.relative(to: target.size);
-    final followerAnchor = follower.anchor.relative(to: follower.box.size);
+    final followerAnchor = follower.anchor.relative(to: follower.size);
     return targetAnchor - followerAnchor;
   }
 }
