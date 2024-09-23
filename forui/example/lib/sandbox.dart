86f55bff9e781a4d00698c3742457519c33724e4
Daviiddoo <Davidchew12345@gmail.com>
2024-09-23 22:21:22 +0800

    Testing new controller

--- forui/example/lib/sandbox.dart
+++ forui/example/lib/sandbox.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:forui/forui.dart';
+import 'package:forui/src/widgets/line_calendar.dart';
 
 class Sandbox extends StatefulWidget {
   const Sandbox({super.key});
@@ -12,6 +13,8 @@ class Sandbox extends StatefulWidget {
 class _SandboxState extends State<Sandbox> {
   bool value = false;
   FSelectGroupController selectGroupController = FRadioSelectGroupController(value: 1);
+
+  final selected = ValueNotifier(DateTime.now());
 
   @override
   void initState() {
@@ -29,6 +32,26 @@ class _SandboxState extends State<Sandbox> {
           ),
           const SizedBox(height: 20),
           const FTextField.password(),
+          FLineCalendar(selected: selected),
+          const SizedBox(height: 20),
+          FTooltip(
+            longPressExitDuration: const Duration(seconds: 5000),
+            tipBuilder: (context, style, _) => const Text('Add to library'),
+            child: FButton(
+              style: FButtonStyle.outline,
+              onPress: () {},
+              label: const Text('Hover'),
+            ),
+          ),
+          Tooltip(
+            message: 'Add to library 2',
+            showDuration: const Duration(seconds: 5000),
+            child: FButton(
+              style: FButtonStyle.outline,
+              onPress: () {},
+              label: const Text('Hover 2'),
+            ),
+          ),
         ],
       );
 }
