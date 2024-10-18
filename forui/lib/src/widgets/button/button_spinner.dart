f3e04ae1998cf236634b5093e24605972fe44d17
Daviiddoo <Daviiddoo@users.noreply.github.com>
2024-10-18 06:51:32 +0000

    Commit from GitHub Actions (Forui Presubmit)

--- forui/lib/src/widgets/button/button_spinner.dart
+++ forui/lib/src/widgets/button/button_spinner.dart
@@ -2,8 +2,10 @@ import 'dart:math' as math;
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:forui/forui.dart';
+
 import 'package:meta/meta.dart';
+
+import 'package:forui/forui.dart';
 
 /// An animated spinner icon.
 ///
