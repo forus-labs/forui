27e0d29877d4e02e6ad1cc32af4566983017e2bc
Pante <Pante@users.noreply.github.com>
2024-10-22 10:03:56 +0000

    Commit from GitHub Actions (Forui Samples Presubmit)

--- samples/lib/foundation/inkwell.dart
+++ samples/lib/foundation/inkwell.dart
@@ -13,18 +13,18 @@ class InkwellPage extends SampleScaffold {
 
   @override
   Widget child(BuildContext context) => FInkwell.animated(
-    builder: (context, data, child) => Container(
-      decoration: BoxDecoration(
-        color: data.hovered ? context.theme.colorScheme.secondary : context.theme.colorScheme.background,
-        borderRadius: BorderRadius.circular(8),
-        border: Border.all(
-          color: context.theme.colorScheme.border,
-        )
-      ),
-      padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 12),
-      child: child!,
-    ),
-    child: const Text('Ink Well'),
-    onPress: () {},
-  );
+        builder: (context, data, child) => Container(
+          decoration: BoxDecoration(
+            color: data.hovered ? context.theme.colorScheme.secondary : context.theme.colorScheme.background,
+            borderRadius: BorderRadius.circular(8),
+            border: Border.all(
+              color: context.theme.colorScheme.border,
+            ),
+          ),
+          padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 12),
+          child: child!,
+        ),
+        child: const Text('Ink Well'),
+        onPress: () {},
+      );
 }
