511fcdd119f79b4829db1a4472ad112227ba8783
Matthias Ngeo <matthiasngeo@gmail.com>
2024-11-02 12:17:20 +0800

    Fix lib name

--- forui/lib/widgets/popover_menu.dart
+++ forui/lib/widgets/popover_menu.dart
@@ -3,6 +3,6 @@
 /// A popover menu displays a menu in a portal aligned to a child.
 ///
 /// See https://forui.dev/docs/overlay/popover-menu for working examples.
-library forui.widgets.popover;
+library forui.widgets.popover_menu;
 
 export '../src/widgets/popover_menu.dart';
