35c4370c8abc499644892bda81071093d608f625
Daviiddoo <Daviiddoo@users.noreply.github.com>
2024-11-04 09:02:21 +0000

    Commit from GitHub Actions (Forui Presubmit)

--- forui/lib/src/theme/theme_data.dart
+++ forui/lib/src/theme/theme_data.dart
@@ -5,7 +5,6 @@ import 'package:meta/meta.dart';
 
 import 'package:forui/forui.dart';
 import 'package:forui/src/theme/breakpoints.dart';
-import 'package:forui/src/widgets/line_calendar/line_calendar.dart';
 
 /// Defines the configuration of the overall visual [FTheme] for a widget subtree.
 ///
