f8147ed57b22fef5c61881e212c75d4e4df1677e
Pante <Pante@users.noreply.github.com>
2024-11-28 13:30:51 +0000

    Commit from GitHub Actions (Forui Samples Presubmit)

--- samples/lib/widgets/line_calendar.dart
+++ samples/lib/widgets/line_calendar.dart
@@ -1,7 +1,9 @@
-import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+
+import 'package:auto_route/auto_route.dart';
 import 'package:forui/forui.dart';
+
 import 'package:forui_samples/sample.dart';
 
 @RoutePage()
