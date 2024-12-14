8ea80ef703b02c683d540dc2348c77e32e046866
Joe <kawaijoe@gmail.com>
2024-12-14 16:59:10 +0800

    Fix rtl locale

--- forui/example/lib/main.dart
+++ forui/example/lib/main.dart
@@ -40,6 +40,7 @@ class _ApplicationState extends State<Application> with SingleTickerProviderStat
 
   @override
   Widget build(BuildContext context) => MaterialApp(
+        // locale: const Locale('ar'),
         localizationsDelegates: FLocalizations.localizationsDelegates,
         supportedLocales: FLocalizations.supportedLocales,
         builder: (context, child) => FTheme(
