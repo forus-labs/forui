beb77db131411713b680b61b3b4f80b173028c7a
Matthias Ngeo <matthiasngeo@gmail.com>
2025-03-11 14:56:52 +0800

    Fix action spacing

--- forui/lib/src/widgets/header/root_header.dart
+++ forui/lib/src/widgets/header/root_header.dart
@@ -48,7 +48,7 @@ final class _FRootHeader extends FHeader {
               ),
               FHeaderData(
                 actionStyle: style.actionStyle,
-                child: Row(children: actions.expand((action) => [action, const SizedBox(width: 10)]).toList()),
+                child: Row(children: actions.expand((action) => [action, SizedBox(width: style.actionSpacing)]).toList()),
               ),
             ],
           ),
