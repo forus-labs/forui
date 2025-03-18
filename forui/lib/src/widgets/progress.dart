85e204091031826ddf4525792ffda14bace957ae
Matthias Ngeo <matthiasngeo@gmail.com>
2025-03-18 15:06:45 +0800

    Add overhauled linear progress

--- forui/lib/src/widgets/progress.dart
+++ forui/lib/src/widgets/progress.dart
@@ -7,100 +7,194 @@ import 'package:forui/forui.dart';
 
 part 'progress.style.dart';
 
-/// A progress bar.
-///
-/// Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.
+/// A progress indicator that shows the completion progress of a task.
 ///
 /// See:
 /// * https://forui.dev/docs/navigation/progress for working examples.
-/// * [FProgressStyle] for customizing a progress's appearance.
-class FProgress extends StatelessWidget {
-  /// The style. Defaults to [FThemeData.progressStyle].
-  final FProgressStyle? style;
-
-  /// If non-null, the value of this progress indicator.
+/// * [FLinearProgressStyle] for customizing a linear progress indicator's appearance.
+abstract class FProgress extends StatefulWidget {
+  /// The progress's value. Defaults to null.
   ///
   /// A value of 0.0 means no progress and 1.0 means that progress is complete.
   /// The value will be clamped to be in the range, `[0.0, 1.0]`.
   ///
+  /// A null value indicates that the the progress should repeat indefinitely.
+  ///
   /// ## Contract
-  /// Throws [AssertionError] if [value] is NaN
-  final double value;
+  /// Throws [AssertionError] if [value] is not null and is less than 0.0 or greater than 1.0.
+  final double? value;
 
-  /// Creates a [FProgress].
-  FProgress({required this.value, this.style, super.key}) : assert(!value.isNaN, 'Cannot provide a NaN value.');
+  /// Creates a linear [FProgress].
+  ///
+  /// The [constraints] is the constraints for the progress bar. Defaults to a height of 15.0 and no horizontal
+  /// constraint.
+  ///
+  /// The [duration] is the duration of the animation. Defaults to 3s if [value] is null and 0.5s otherwise.
+  const factory FProgress({
+    FLinearProgressStyle? style,
+    double? value,
+    BoxConstraints constraints,
+    Duration duration,
+    Key? key,
+  }) = _Linear;
+
+  const FProgress._({this.value, super.key})
+    : assert(value == null || value >= 0.0, 'The value must be greater than or equal to 0.0'),
+      assert(value == null || value <= 1.0, 'The value must be less than or equal to 1.0');
 
   @override
-  Widget build(BuildContext context) {
-    final style = this.style ?? context.theme.progressStyle;
-    final value = switch (this.value) {
-      < 0.0 => 0.0,
-      > 1.0 => 1.0,
-      _ => this.value,
-    };
-
-    return LayoutBuilder(
-      builder:
-          (_, constraints) => ConstrainedBox(
-            constraints: style.constraints,
-            child: Stack(
-              alignment: AlignmentDirectional.centerStart,
-              children: [
-                Container(decoration: style.backgroundDecoration, width: constraints.maxWidth),
-                AnimatedContainer(
-                  curve: style.curve,
-                  duration: style.animationDuration,
-                  decoration: style.progressDecoration,
-                  width: value * constraints.maxWidth,
-                ),
-              ],
-            ),
-          ),
-    );
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties.add(DoubleProperty('value', value));
   }
+}
+
+class _Linear extends FProgress {
+  static const _infinite = Duration(milliseconds: 1500);
+  static const _finite = Duration(milliseconds: 500);
+
+  final FLinearProgressStyle? style;
+  final Duration duration;
+  final BoxConstraints constraints;
+
+  const _Linear({
+    this.style,
+    this.constraints = const BoxConstraints(minHeight: 10.0, maxHeight: 10.0),
+    Duration? duration,
+    super.value,
+    super.key,
+  }) : duration = duration ?? (value == null ? _infinite : _finite),
+       super._();
+
+  @override
+  State<_Linear> createState() => _LinearState();
 
   @override
   void debugFillProperties(DiagnosticPropertiesBuilder properties) {
     super.debugFillProperties(properties);
     properties
       ..add(DiagnosticsProperty('style', style))
-      ..add(PercentProperty('value', value));
+      ..add(DiagnosticsProperty('duration', duration))
+      ..add(DiagnosticsProperty('constraints', constraints));
   }
 }
 
+class _LinearState extends State<_Linear> with SingleTickerProviderStateMixin {
+  late final AnimationController _controller;
+  late CurvedAnimation _curve;
+  late Animation<double> _animation;
+  double _previous = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this, duration: widget.duration);
+    widget.value == null ? _controller.repeat() : _controller.forward();
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    final style = widget.style ?? context.theme.progressStyles.linearProgressStyle;
+    _curve = CurvedAnimation(parent: _controller, curve: style.curve);
+    _animation = Tween(begin: _previous, end: widget.value ?? 1).animate(_curve);
+  }
+
+  @override
+  void didUpdateWidget(covariant _Linear old) {
+    super.didUpdateWidget(old);
+    var reanimate = false;
+    if (widget.duration != old.duration) {
+      _controller.duration = widget.duration;
+      reanimate = true;
+    }
+
+    if (widget.value != old.value) {
+      _previous = widget.value == null ? 0 : (old.value ?? 0);
+      reanimate = true;
+    }
+
+    if (widget.style != old.style) {
+      final style = widget.style ?? context.theme.progressStyles.linearProgressStyle;
+      _curve = CurvedAnimation(parent: _controller, curve: style.curve);
+      reanimate = true;
+    }
+
+    if (reanimate) {
+      _animation = Tween(begin: _previous, end: widget.value ?? 1).animate(_curve);
+      if (widget.value == null) {
+        _controller.repeat();
+      } else {
+        _controller
+          ..reset()
+          ..forward();
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final style = widget.style ?? context.theme.progressStyles.linearProgressStyle;
+    return ConstrainedBox(
+      constraints: widget.constraints,
+      child: DecoratedBox(
+        decoration: style.backgroundDecoration,
+        child: Align(
+          alignment: AlignmentDirectional.centerStart,
+          child: AnimatedBuilder(
+            animation: _animation,
+            builder: (_, child) => FractionallySizedBox(widthFactor: _animation.value, child: child!),
+            child: Container(decoration: style.progressDecoration),
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+}
+
+/// The progress styles.
+class FProgressStyles with Diagnosticable, _$FProgressStylesFunctions {
+  /// The linear progress's style.
+  @override
+  final FLinearProgressStyle linearProgressStyle;
+
+  /// Creates a [FProgressStyles].
+  const FProgressStyles({required this.linearProgressStyle});
+
+  /// Creates a [FProgressStyles] that inherits its properties from [colorScheme] and [style].
+  FProgressStyles.inherit({required FColorScheme colorScheme, required FStyle style})
+    : linearProgressStyle = FLinearProgressStyle.inherit(colorScheme: colorScheme, style: style);
+}
+
 /// [FProgress]'s style.
-final class FProgressStyle with Diagnosticable, _$FProgressStyleFunctions {
-  /// The background's color.
+class FLinearProgressStyle with Diagnosticable, _$FLinearProgressStyleFunctions {
+  /// The progress's background's decoration.
   @override
   final BoxDecoration backgroundDecoration;
 
-  /// The progress's color.
+  /// The progress's decoration.
   @override
   final BoxDecoration progressDecoration;
-
-  /// The constraints for the progress bar. Defaults to a height of 15.0.
-  @override
-  final BoxConstraints constraints;
-
-  /// The animation duration. Defaults to 500ms.
-  @override
-  final Duration animationDuration;
 
   /// The animation curve. Defaults to [Curves.ease].
   @override
   final Curve curve;
 
-  /// Creates a [FProgressStyle].
-  const FProgressStyle({
+  /// Creates a [FLinearProgressStyle].
+  const FLinearProgressStyle({
     required this.backgroundDecoration,
     required this.progressDecoration,
-    this.constraints = const BoxConstraints(minHeight: 15.0, maxHeight: 15.0),
-    this.animationDuration = const Duration(milliseconds: 500),
     this.curve = Curves.ease,
   });
 
-  /// Creates a [FProgressStyle] that inherits its properties from [colorScheme] and [style].
-  FProgressStyle.inherit({required FColorScheme colorScheme, required FStyle style})
+  /// Creates a [FLinearProgressStyle] that inherits its properties from [colorScheme] and [style].
+  FLinearProgressStyle.inherit({required FColorScheme colorScheme, required FStyle style})
     : this(
         backgroundDecoration: BoxDecoration(borderRadius: style.borderRadius, color: colorScheme.secondary),
         progressDecoration: BoxDecoration(borderRadius: style.borderRadius, color: colorScheme.primary),
--- forui/test/src/widgets/progress_golden_test.dart
+++ forui/test/src/widgets/progress_golden_test.dart
@@ -7,23 +7,49 @@ import 'package:forui/forui.dart';
 import '../test_scaffold.dart';
 
 void main() {
-  group('FProgress', () {
+  group('linear', () {
     testWidgets('blue screen', (tester) async {
       await tester.pumpWidget(
-        TestScaffold.blue(child: FProgress(style: TestScaffold.blueScreen.progressStyle, value: 0.5)),
+        TestScaffold.blue(
+          child: FProgress(style: TestScaffold.blueScreen.progressStyles.linearProgressStyle, value: 0.5),
+        ),
       );
 
       await expectBlueScreen(find.byType(TestScaffold));
     });
 
     for (final theme in TestScaffold.themes) {
-      for (final (progress, value) in [('positive', 17.0), ('negative', -4.0), ('clamped', 0.3)]) {
-        testWidgets('${theme.name} - $progress', (tester) async {
+      for (final value in [0.0, 0.5, 1.0]) {
+        testWidgets('${theme.name} - $value', (tester) async {
           await tester.pumpWidget(TestScaffold(theme: theme.data, child: FProgress(value: value)));
+          await tester.pumpAndSettle();
 
-          await expectLater(find.byType(TestScaffold), matchesGoldenFile('progress/${theme.name}-$progress.png'));
+          await expectLater(find.byType(TestScaffold), matchesGoldenFile('progress/linear/${theme.name}/$value.png'));
         });
       }
+
+      testWidgets('${theme.name} - indefinite', (tester) async {
+        await tester.pumpWidget(TestScaffold(theme: theme.data, child: const FProgress()));
+        await tester.pump(const Duration(milliseconds: 500));
+
+        await expectLater(find.byType(TestScaffold), matchesGoldenFile('progress/linear/${theme.name}/indefinite.png'));
+      });
     }
+
+    testWidgets('update value double', (tester) async {
+      await tester.pumpWidget(TestScaffold(child: const FProgress(value: 0.5)));
+      await tester.pumpWidget(TestScaffold(child: const FProgress(value: 1)));
+      await tester.pumpAndSettle();
+
+      await expectLater(find.byType(TestScaffold), matchesGoldenFile('progress/linear/update-value-double.png'));
+    });
+
+    testWidgets('update value to null', (tester) async {
+      await tester.pumpWidget(TestScaffold(child: const FProgress(value: 1)));
+      await tester.pumpWidget(TestScaffold(child: const FProgress()));
+      await tester.pump(const Duration(milliseconds: 300));
+
+      await expectLater(find.byType(TestScaffold), matchesGoldenFile('progress/linear/update-value-numer.png'));
+    });
   });
 }
