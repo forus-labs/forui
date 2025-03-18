3214a0578f87bb3a5729de1da4c275eb5518f3cf
Pante <Pante@users.noreply.github.com>
2025-03-18 15:34:08 +0000

    Commit from GitHub Actions (Forui Presubmit)

--- forui/lib/src/widgets/button/button.dart
+++ forui/lib/src/widgets/button/button.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
-import 'package:forui/src/widgets/progress.dart';
 
 import 'package:meta/meta.dart';
 
 import 'package:forui/forui.dart';
 import 'package:forui/src/widgets/button/button_content.dart';
+import 'package:forui/src/widgets/progress.dart';
 
 part 'button.style.dart';
 
