5ac8b71c579e43c141d5140beb30ef1274141260
Pante <Pante@users.noreply.github.com>
2025-03-20 14:58:38 +0000

    Commit from GitHub Actions (Forui Samples Presubmit)

--- samples/lib/widgets/select.dart
+++ samples/lib/widgets/select.dart
@@ -1,6 +1,8 @@
-import 'package:auto_route/annotations.dart';
 import 'package:flutter/widgets.dart';
+
+import 'package:auto_route/annotations.dart';
 import 'package:forui/forui.dart';
+
 import 'package:forui_samples/sample.dart';
 
 const fruits = [
