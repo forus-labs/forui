961c6ebfafc367d44842f40524185366aab56945
Matthias Ngeo <matthiasngeo@gmail.com>
2025-03-21 14:36:57 +0800

    Fix failing build

--- samples/lib/main.dart
+++ samples/lib/main.dart
@@ -105,7 +105,7 @@ class _AppRouter extends RootStackRouter {
     AutoRoute(path: '/select/async-error', page: AsyncErrorSelectRoute.page),
     AutoRoute(path: '/select/clearable', page: ClearableSelectRoute.page),
     AutoRoute(path: '/select/format', page: FormatSelectRoute.page),
-    AutoRoute(path: '/select/no-scroll-handles', page: NoScrollHandlesSelectRoute.page),
+    AutoRoute(path: '/select/scroll-handles', page: ScrollHandlesSelectRoute.page),
     AutoRoute(path: '/select/form', page: FormSelectRoute.page),
     AutoRoute(path: '/select-group/default', page: SelectGroupRoute.page),
     AutoRoute(path: '/select-group/checkbox-form', page: SelectGroupCheckboxFormRoute.page),
