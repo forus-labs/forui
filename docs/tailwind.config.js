4fb1d203ee2103eb1f61dfa72a5e8b87c4289205
Joe <kawaijoe@gmail.com>
2025-04-04 13:44:59 +0800

    Upgrade nextra to v4

--- docs/tailwind.config.js
+++ docs/tailwind.config.js
@@ -1,14 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx,md,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,md,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,md,mdx}",
-  ],
-  darkMode: 'selector',
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: [
+        './app/**/*.{js,jsx,ts,tsx,md,mdx}',
+        './content/**/*.{md,mdx}',
 
+        // Or if using `src` directory:
+        './src/**/*.{js,jsx,ts,tsx,md,mdx}'
+    ],
+    theme: {
+        extend: {}
+    },
+    plugins: []
+};
