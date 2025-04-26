b465127670db9970604d4c6dffe58bcd40c31268
Pante <Pante@users.noreply.github.com>
2025-04-26 12:04:16 +0000

    Commit from GitHub Actions (Forui Presubmit)

--- forui/test/src/widgets/dialog/dialog_test.dart
+++ forui/test/src/widgets/dialog/dialog_test.dart
@@ -7,38 +7,30 @@ import '../../test_scaffold.dart';
 
 void main() {
   group('FDialog', () {
-      for (final direction in Axis.values) {
-        testWidgets('$direction infinite sized child', (tester) async {
-          await tester.pumpWidget(
-            TestScaffold(
-              child: FDialog(
-                direction: direction,
-                title: const Text('Are you absolutely sure?'),
-                body: SingleChildScrollView(
-                  child: Text.rich(
-                    WidgetSpan(
-                      child: Stack(
-                        children: [
-                          Container(
-                            height: 200,
-                            width: double.infinity,
-                            color: Colors.red,
-                          ),
-                        ],
-                      ),
-                    ),
+    for (final direction in Axis.values) {
+      testWidgets('$direction infinite sized child', (tester) async {
+        await tester.pumpWidget(
+          TestScaffold(
+            child: FDialog(
+              direction: direction,
+              title: const Text('Are you absolutely sure?'),
+              body: SingleChildScrollView(
+                child: Text.rich(
+                  WidgetSpan(
+                    child: Stack(children: [Container(height: 200, width: double.infinity, color: Colors.red)]),
                   ),
                 ),
-                actions: [
-                  FButton(child: const Text('Continue'), onPress: () {}),
-                  FButton(style: FButtonStyle.outline, child: const Text('Cancel'), onPress: () {}),
-                ],
               ),
+              actions: [
+                FButton(child: const Text('Continue'), onPress: () {}),
+                FButton(style: FButtonStyle.outline, child: const Text('Cancel'), onPress: () {}),
+              ],
             ),
-          );
+          ),
+        );
 
-          expect(tester.takeException(), null);
-        });
-      }
+        expect(tester.takeException(), null);
+      });
+    }
   });
 }
