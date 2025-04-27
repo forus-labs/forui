85adbb1a64ae57afdcde9fcab76ee9a74a91184a
Matthias Ngeo <matthiasngeo@gmail.com>
2025-04-27 09:18:29 +0800

    Fix typos

--- forui/example/lib/sandbox.dart
+++ forui/example/lib/sandbox.dart
@@ -10,70 +10,18 @@ class Sandbox extends StatefulWidget {
 }
 
 class _SandboxState extends State<Sandbox> {
-  int pages = 10;
-  PageController controller = PageController();
-  late FPaginationController paginationController = FPaginationController(pages: pages);
-
-  @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-    final value = PageStorage.maybeOf(context)?.readState(context) ?? 0;
-    paginationController.page = value;
-  }
-
-  void _handlePageChanged() {
-    if (!controller.hasClients) {
-      return;
-    }
-    final page = paginationController.page;
-    final old = controller.page?.round();
-    if (old case final old when old != page) {
-      if (page == old! + 1 || page == old - 1) {
-        controller.animateToPage(page, duration: const Duration(milliseconds: 300), curve: Curves.easeInOut);
-      } else {
-        controller.jumpToPage(page);
-      }
-    }
-  }
 
   @override
   Widget build(BuildContext context) => Column(
     mainAxisSize: MainAxisSize.min,
     mainAxisAlignment: MainAxisAlignment.center,
     spacing: 10,
-    children: [
+    children: const [
       SizedBox(
         height: 300,
         width: 300,
-        child: NotificationListener(
-          onNotification: (notification) {
-            if (notification is ScrollEndNotification) {
-              if (controller.hasClients) {
-                paginationController.page = controller.page!.round();
-                return true;
-              }
-            }
-            return false;
-          },
-          child: PageView.builder(
-            itemCount: pages,
-            controller: controller,
-            itemBuilder:
-                (context, index) => ColoredBox(
-                  color: index.isEven ? Colors.red : Colors.blue,
-                  child: Center(child: Text('Page $index', style: const TextStyle(fontSize: 45, color: Colors.white))),
-                ),
-          ),
-        ),
       ),
-      SizedBox(height: 200, child: FPagination(controller: paginationController, onChange: _handlePageChanged)),
+      SizedBox(height: 200, child: FPagination()),
     ],
   );
-
-  @override
-  void dispose() {
-    paginationController.dispose();
-    controller.dispose();
-    super.dispose();
-  }
 }
