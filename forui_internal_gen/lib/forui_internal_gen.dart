54a4ffc9ff54d2522b8ad8099f49f71b12088d97
Matthias Ngeo <matthiasngeo@gmail.com>
2025-04-29 11:10:44 +0800

    Add codecov tool

--- forui_internal_gen/lib/forui_internal_gen.dart
+++ forui_internal_gen/lib/forui_internal_gen.dart
@@ -3,9 +3,17 @@ import 'package:dart_style/dart_style.dart';
 import 'package:forui_internal_gen/src/style_generator.dart';
 import 'package:source_gen/source_gen.dart';
 
+const _header = '''
+$defaultFileHeader
+
+// dart format width=120
+// coverage:ignore-file
+''';
+
 /// Builds generators for `build_runner` to run
 Builder styleBuilder(BuilderOptions options) => PartBuilder(
   [StyleGenerator()],
   '.style.dart',
+  header: _header,
   formatOutput: (generated, version) => DartFormatter(pageWidth: 120, languageVersion: version).format(generated),
 );
