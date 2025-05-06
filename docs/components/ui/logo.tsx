6e9ae0980e64a7b86a636502ddf58cbd82f82eb2
Joe Kawai <kawaijoe@users.noreply.github.com>
2025-05-07 00:36:42 +0800

    Fix document logo hydration error (#517)
    * Fix
    
    * Fixed pr issues
    
    * Update github action
    
    * Update lint.yaml (#518)
    
    * chore(deps): update dependency gradle to v8.14 (#506)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * Reactive md lint
    
    * Fix
    
    * Fixed pr issues
    
    * Update github action
    
    * Reactive md lint
    
    ---------
    
    Co-authored-by: Matthias Ngeo <matthiasngeo@gmail.com>
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>

--- docs/components/ui/logo.tsx
+++ docs/components/ui/logo.tsx
@@ -2,6 +2,7 @@
 
 import { useTheme } from 'nextra-theme-docs';
 import Image from 'next/image';
+import { useState, useEffect } from 'react';
 
 interface Props {
   className?: string;
@@ -9,10 +10,19 @@ interface Props {
 
 export default function Logo({ className = 'w-24 h-auto' }: Props) {
   const { resolvedTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
 
   return (
     <Image
-      src={resolvedTheme == 'dark' ? '/dark_logo.svg' : '/light_logo.svg'}
+      src={resolvedTheme === 'dark' ? '/dark_logo.svg' : '/light_logo.svg'}
       className={className}
       width={0}
       height={0}
