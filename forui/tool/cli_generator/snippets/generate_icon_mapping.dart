a0069954fccb4c1319629f077a539298742ea7d6
Matthias Ngeo <matthiasngeo@gmail.com>
2025-05-24 22:00:11 +0800

    Add icon-mapping snippet

---
+++ forui/tool/cli_generator/snippets/generate_icon_mapping.dart
@@ -0,0 +1,76 @@
+import 'package:analyzer/dart/ast/ast.dart';
+import 'package:analyzer/dart/ast/visitor.dart';
+import 'package:code_builder/code_builder.dart';
+
+import '../main.dart';
+
+const iconMapping = 'icon-mapping';
+
+String mapIconMapping(List<String> icons) {
+  final library = LibraryBuilder()
+    ..comments.addAll([header])
+    ..body.addAll([
+      (EnumBuilder()
+            ..docs.addAll([
+              '/// Provides a mapping for [FIcons] which can be iterated over using [EnumByName].',
+              '///',
+              '/// ## Note',
+              '/// This enum may prevent [FIcons] from being tree-shaken and result in larger bundle sizes.',
+            ])
+            ..name = 'FIconMapping'
+            ..values.addAll([
+              for (final icon in icons)
+                (EnumValueBuilder()
+                      ..name = icon
+                      ..arguments.addAll([refer('FIcons.$icon')]))
+                    .build(),
+            ])
+            ..fields.addAll([
+              (FieldBuilder()
+                    ..docs.addAll(['/// The icon data.'])
+                    ..name = 'icon'
+                    ..type = refer('IconData')
+                    ..modifier = FieldModifier.final$)
+                  .build(),
+            ])
+            ..constructors.add(
+              (ConstructorBuilder()
+                    ..constant = true
+                    ..requiredParameters.addAll([Parameter((p) => p..name = 'this.icon')]))
+                  .build(),
+            ))
+          .build(),
+    ]);
+
+  final code =
+      '''
+import 'package:flutter/widgets.dart';
+import 'package:forui/forui.dart';
+
+${library.build().accept(emitter)}
+''';
+  return formatter.format(code);
+}
+
+/// A visitor that finds all static const fields in a class.
+class IconsFinder extends RecursiveAstVisitor<void> {
+  final List<String> icons = [];
+
+  @override
+  void visitClassDeclaration(ClassDeclaration declaration) {
+    final name = declaration.name.lexeme;
+    if (name == 'FIcons') {
+      super.visitClassDeclaration(declaration);
+    }
+  }
+
+  @override
+  void visitFieldDeclaration(FieldDeclaration declaration) {
+    if (declaration.isStatic && declaration.fields.isConst) {
+      for (final variable in declaration.fields.variables) {
+        icons.add(variable.name.lexeme);
+      }
+    }
+    super.visitFieldDeclaration(declaration);
+  }
+}
---
+++ forui/tool/cli_generator/snippets/generate_material_mapping.dart
@@ -0,0 +1,44 @@
+import 'package:analyzer/dart/ast/ast.dart';
+import 'package:analyzer/dart/ast/visitor.dart';
+
+import '../main.dart';
+
+const materialThemeMapping = 'material-mapping';
+
+String mapMaterialThemeMapping(String method) => formatter.format('''
+import 'package:flutter/material.dart';
+import 'package:forui/forui.dart';
+
+extension CustomMaterialTheme on FThemeData {
+  // Modify this method to customize how FThemeData is mapped to Material ThemeData.
+  // 
+  // To use this method:
+  // ```dart
+  // final theme = FTheme.zinc.light;
+  //
+  // MaterialApp(
+  //   theme: theme.toCustomMaterialTheme(),
+  // );
+  // ```
+  ${method.replaceAll('toApproximateMaterialTheme', 'toCustomMaterialTheme').replaceAll('@experimental', '')}
+}
+''');
+
+class ToApproximateMaterialThemeFinder extends RecursiveAstVisitor<void> {
+  String? snippet;
+
+  @override
+  void visitClassDeclaration(ClassDeclaration declaration) {
+    final name = declaration.name.lexeme;
+    if (name == 'FThemeData') {
+      super.visitClassDeclaration(declaration);
+    }
+  }
+
+  @override
+  void visitMethodDeclaration(MethodDeclaration declaration) {
+    if (declaration.name.lexeme == 'toApproximateMaterialTheme') {
+      snippet = declaration.toSource();
+    }
+  }
+}
