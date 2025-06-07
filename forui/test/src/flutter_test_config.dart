6b5d88633974eac317a5c9fd9a566fd693ff1697
Matthias Ngeo <matthiasngeo@gmail.com>
2025-06-07 10:23:27 +0800

    let's go

--- forui/test/src/flutter_test_config.dart
+++ forui/test/src/flutter_test_config.dart
@@ -6,35 +6,24 @@ import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:leak_tracker_flutter_testing/leak_tracker_flutter_testing.dart';
 
+import 'test_scaffold.dart';
+
 Future<void> testExecutable(FutureOr<void> Function() testMain) async {
+  WidgetController.hitTestWarningShouldBeFatal = true;
   LeakTesting.enable();
   LeakTesting.settings = LeakTesting.settings.withIgnored(
     createdByTestHelpers: true,
     classes: ['Image', 'ImageInfo', 'ImageStreamCompleterHandle', '_CachedImage'],
   );
 
-  await configureGoldenTests();
-  await testMain();
-}
-
-Future<void> configureGoldenTests() async {
-  TestWidgetsFlutterBinding.ensureInitialized();
-
-  final workingDirectory = Directory.current.path.contains('forui${Platform.pathSeparator}forui')
-      ? '.'
-      : '${Directory.current.path}/forui';
-
   final inter = FontLoader('packages/forui/Inter');
-  final directory = Directory('$workingDirectory/assets/fonts/inter/');
-  for (final file in directory.listSync().whereType<File>().where((e) => e.path.endsWith('.ttf'))) {
+  for (final file in Directory('$relativePath/assets/fonts/inter/').listSync().whereType<File>()) {
     inter.addFont(rootBundle.load(file.path));
   }
-
   await inter.load();
 
   final lucide = FontLoader('packages/forui_assets/ForuiLucideIcons')
     ..addFont(rootBundle.load('packages/forui_assets/assets/lucide.ttf'));
-
   await lucide.load();
 
   if (goldenFileComparator is LocalFileComparator) {
@@ -45,7 +34,9 @@ Future<void> configureGoldenTests() async {
       // As such, we use the default `testUrl`, which is only the `baseDir` and
       // append a generically named `test.dart` so that the `baseDir` is
       // properly extracted.
-      Uri.parse('$workingDirectory/test/golden/${Platform.operatingSystem}/test.dart'),
+      Uri.parse('$relativePath/test/golden/${Platform.operatingSystem}/test.dart'),
     );
   }
+
+  await testMain();
 }
