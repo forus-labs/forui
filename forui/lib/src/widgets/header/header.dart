0d85bab969dd6c0af1440edcb718a70a2c9ada95
Pante <Pante@users.noreply.github.com>
2025-10-03 03:53:16 +0000

    Prepare Forui for review

--- forui/lib/src/widgets/header/header.dart
+++ forui/lib/src/widgets/header/header.dart
@@ -5,13 +5,12 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
-import 'package:forui/src/foundation/rendering.dart';
 
 import 'package:meta/meta.dart';
-import 'package:sugar/collection.dart';
 
 import 'package:forui/forui.dart';
 import 'package:forui/src/foundation/debug.dart';
+import 'package:forui/src/foundation/rendering.dart';
 
 part 'header.design.dart';
 
