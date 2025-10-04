b70c1941614795448dfa062830222afe304228d6
Pante <Pante@users.noreply.github.com>
2025-10-04 04:43:03 +0000

    Prepare Forui for review

--- forui/test/src/widgets/progresses/determinate_progress_test.dart
+++ forui/test/src/widgets/progresses/determinate_progress_test.dart
@@ -1,5 +1,3 @@
-import 'package:flutter/material.dart';
-
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:forui/forui.dart';
@@ -10,7 +8,7 @@ void main() {
     await tester.pumpWidget(TestScaffold(theme: FThemes.zinc.light, child: const FDeterminateProgress(value: 0.5)));
     await tester.pump();
 
-    await tester.pumpWidget(TestScaffold(theme: FThemes.zinc.dark, child: const FDeterminateProgress(value: 0.6,)));
+    await tester.pumpWidget(TestScaffold(theme: FThemes.zinc.dark, child: const FDeterminateProgress(value: 0.6)));
     await tester.pump();
 
     expect(tester.takeException(), null);
--- forui/test/src/widgets/progresses/progress_test.dart
+++ forui/test/src/widgets/progresses/progress_test.dart
@@ -1,5 +1,3 @@
-import 'package:flutter/material.dart';
-
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:forui/forui.dart';
