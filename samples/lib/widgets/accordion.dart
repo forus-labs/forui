81a4d315e47e0e3d21ba695a483ded28a4090cf8
Matthias Ngeo <matthiasngeo@gmail.com>
2025-10-29 12:27:49 +0800

    Update

--- samples/lib/widgets/accordion.dart
+++ samples/lib/widgets/accordion.dart
@@ -23,7 +23,7 @@ class AccordionPage extends Sample {
           controller: controller,
           children: const [
             FAccordionItem(
-              title: Text('TEST Information 2'),
+              title: Text('TEST Information 3'),
               child: Text('''
 Our flagship product combines cutting-edge technology with sleek design. Built with premium materials, it offers unparalleled performance and reliability.
 
