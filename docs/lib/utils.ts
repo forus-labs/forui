f8c60be3865f4b7e0736217171e05182f2dada97
Matthias Ngeo <matthiasngeo@gmail.com>
2025-10-29 11:40:21 +0800

    whoops

--- docs/lib/utils.ts
+++ docs/lib/utils.ts
@@ -1,8 +1,6 @@
 import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
-let demoUrl: string | null = null;
-
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
