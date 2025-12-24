aec80996338c794adf72b6d8dbe2cb4470b07748
Matthias Ngeo <matthiasngeo@gmail.com>
2025-12-24 11:57:18 +0800

    Add initial sample transformation

---
+++ samples/tool/snippet_generator/main.dart
@@ -0,0 +1,185 @@
+import 'dart:collection';
+import 'dart:io';
+
+import 'package:analyzer/dart/analysis/analysis_context_collection.dart';
+import 'package:analyzer/dart/analysis/results.dart';
+import 'package:analyzer/dart/analysis/session.dart';
+import 'package:analyzer/dart/ast/ast.dart';
+import 'package:analyzer/dart/ast/visitor.dart';
+import 'package:analyzer/dart/constant/value.dart';
+import 'package:analyzer/dart/element/type.dart';
+import 'package:analyzer/file_system/overlay_file_system.dart';
+import 'package:analyzer/file_system/physical_file_system.dart';
+import 'package:collection/collection.dart';
+import 'package:dart_style/dart_style.dart';
+import 'package:path/path.dart' as p;
+
+import 'transformations.dart';
+import 'widget_visitors.dart';
+
+final _forui = p.join(Directory.current.parent.path, 'forui', 'lib');
+final _samples = p.join(Directory.current.parent.path, 'samples', 'lib');
+
+/// The formatter used to format the generated code snippets.
+final formatter = DartFormatter(languageVersion: DartFormatter.latestLanguageVersion);
+
+class Sample {
+  final List<String> routes = [];
+  late String source;
+}
+
+Future<void> main() async {
+  final provider = OverlayResourceProvider(PhysicalResourceProvider.INSTANCE);
+  final collection = AnalysisContextCollection(includedPaths: [_forui, _samples], resourceProvider: provider);
+
+  final routesVisitor = RoutesVisitor();
+  final main = p.join(_samples, 'main.dart');
+  if (await collection.contextFor(main).currentSession.getResolvedUnit(main) case final ResolvedUnitResult result) {
+    result.unit.visitChildren(routesVisitor);
+  }
+
+  final samples = routesVisitor.samples;
+
+  for (final file in Directory(_samples).listSync(recursive: true).whereType<File>()) {
+    if (!file.path.endsWith('.dart')) {
+      continue;
+    }
+
+    final session = collection.contextFor(file.path).currentSession;
+    if (await session.getResolvedUnit(file.path) case final ResolvedUnitResult result) {
+      final sampleVisitor = SampleVisitor(samples, result, session);
+      result.unit.visitChildren(sampleVisitor);
+    }
+  }
+
+  for (final sample in samples.values) {
+    print(sample.source);
+  }
+}
+
+class RoutesVisitor extends RecursiveAstVisitor<void> {
+  /// The extracted routes as a map of page -> list of routes.
+  final Map<String, Sample> samples = {};
+
+  @override
+  void visitClassDeclaration(ClassDeclaration node) {
+    if (node.metadata.any((a) => a.name.name == 'AutoRouterConfig')) {
+      super.visitClassDeclaration(node);
+    }
+  }
+
+  @override
+  void visitInstanceCreationExpression(InstanceCreationExpression node) {
+    if (node.constructorName.type.name.lexeme != 'AutoRoute') {
+      super.visitInstanceCreationExpression(node);
+      return;
+    }
+
+    String? path;
+    String? page;
+    for (final NamedExpression(:name, :expression) in node.argumentList.arguments.whereType<NamedExpression>()) {
+      switch ((name.label.name, expression)) {
+        case ('path', final StringLiteral expression):
+          path = expression.stringValue;
+
+        case ('page', final expression):
+          // page: AccordionRoute.page -> extract 'AccordionPage'
+          page = expression.toSource().replaceAll('Route.page', 'Page');
+      }
+
+      if (path != null && page != null) {
+        (samples[page] ??= Sample()).routes.add(path);
+      }
+    }
+  }
+}
+
+// Assumptions:
+// * inline is always a class declaration.
+// * both the main and inlined widgets are in the same file.
+class SampleVisitor extends RecursiveAstVisitor<Future<void>> {
+  final Map<String, Sample> _samples;
+  final ResolvedUnitResult _result;
+  final AnalysisSession _session;
+
+  SampleVisitor(this._samples, this._result, this._session);
+
+  @override
+  Future<void> visitClassDeclaration(ClassDeclaration node) async {
+    if (node.metadata.none((a) => a.name.name == 'RoutePage')) {
+      return;
+    }
+
+    List<DartType> inclusions = const [];
+    DartType? inline;
+    bool full = false;
+    if (node.metadata.firstWhereOrNull((a) => a.name.name == 'Options') case final annotation?) {
+      final value = annotation.elementAnnotation!.computeConstantValue()!;
+      inclusions = [for (final type in value.getField('include')?.toListValue() ?? <DartObject>[]) ?type.toTypeValue()];
+      inline = value.getField('inline')?.toTypeValue();
+      full = value.getField('full')?.toBoolValue() ?? false;
+    }
+
+    if (inline == null) {
+      (_samples[node.name.lexeme] ??= Sample()).source = await _include(
+        _result,
+        inclusions,
+        node,
+        visitWidget(_result, node, const {}, full: full),
+      );
+    } else {
+      final InlineCallSiteVisitor(:inlines) = InlineCallSiteVisitor(inline)..visitClassDeclaration(node);
+      final (result, type as ClassDeclaration) = (await _session.declaration(inline.element!))!;
+
+      (_samples[node.name.lexeme] ??= Sample()).source = await _include(
+        result,
+        inclusions,
+        node,
+        visitWidget(result, type, inlines, full: full),
+      );
+    }
+  }
+
+  Future<String> _include(
+    ResolvedUnitResult result,
+    List<DartType> inclusions,
+    ClassDeclaration declaration,
+    String widget,
+  ) async {
+    final parts = SplayTreeMap<int, String>()
+      ..[0] = result.unit.directives.whereType<ImportDirective>().map((d) => d.toSource()).join('\n')
+      ..[declaration.offset] = widget;
+
+    for (final type in inclusions) {
+      final (result, declaration) = (await _session.declaration(type.element!))!;
+      parts[declaration.offset] = declaration.toSource();
+    }
+
+    return formatter.format(parts.values.join('\n\n'));
+  }
+}
+
+class InlineCallSiteVisitor extends RecursiveAstVisitor<void> {
+  final Map<String, String> inlines = {};
+  final DartType _type;
+
+  InlineCallSiteVisitor(this._type);
+
+  @override
+  void visitInstanceCreationExpression(InstanceCreationExpression node) {
+    if (_type.element != node.staticType?.element) {
+      super.visitInstanceCreationExpression(node);
+      return;
+    }
+
+    // We don't support M-1 inlines for now.
+    final constructor = node.constructorName.element!;
+    for (final (i, argument) in node.argumentList.arguments.indexed) {
+      if (argument case NamedExpression(:final name, :final expression)) {
+        inlines[name.label.name] = expression.toSource();
+      } else {
+        inlines[constructor.formalParameters[i].name!] = argument.toSource();
+      }
+    }
+  }
+}
