80addc9574ac9c8b2fddb849442f2199fd9d1384
Joe <kawaijoe@gmail.com>
2026-01-08 15:08:16 +0800

    Another attempt

--- docs-legacy/components/ui/link-badge/link-badge-group.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from 'react';
-
-interface Props {
-  children: ReactNode;
-}
-
-export default function LinkBadgeGroup(props: Props) {
-  return <div className="flex gap-2 py-4">{props.children}</div>;
-}
